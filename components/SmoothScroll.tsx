d1f01b2d0af8bc6b12dd73ba9344db94761edd73
Seu Nome <244080428+lucas1112b-prog@users.noreply.github.com>
2025-11-14 18:18:21 -0300

    fix: remove smoothTouch from Lenis options

--- components/SmoothScroll.tsx
+++ components/SmoothScroll.tsx
@@ -5,10 +5,9 @@ import Lenis from "@studio-freight/lenis";
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,     // controle da suavidade
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // ease custom
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
-      smoothTouch: false,
     });
 
     function raf(time: number) {
